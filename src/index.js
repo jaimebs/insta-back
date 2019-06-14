7b8c452a0eb85bab8bfc0bf98e04ec2f62160285
Jaime Barbosa <jaime.barbosa@solucaosistemas.net>
2019-06-14 11:44:11 -0300

    refactor: adding express.urlencoded and express.json, for handling requests. :pencil:

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
